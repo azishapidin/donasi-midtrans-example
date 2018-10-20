285317e5002bf479c6c037444ffd210d30d40eb6
Azis Hapidin <azishapidin@gmail.com>
2018-10-20 09:09:19 +0700

    add midtrans configuration

--- config/services.php
+++ config/services.php
@@ -35,4 +35,15 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'midtrans' => [
+        // Midtrans server key
+        'serverKey'     => env('MIDTRANS_SERVERKEY'),
+        // Midtrans client key
+        'clientKey'     => env('MIDTRANS_CLIENTID'),
+        // Isi false jika masih tahap development dan true jika sudah di production, default false (development)
+        'isProduction'  => env('MIDTRANS_IS_PRODUCTION', false),
+        'isSanitized'   => env('MIDTRANS_IS_SANITIZED', true),
+        'is3ds'         => env('MIDTRANS_IS_3DS', true),                
+    ],
+
 ];
